1f85ffd1872463c0cb519db29e328e392c0ba255
shivakant <shivakantyuvasoft357@gmail.com>
2023-09-18 17:12:54 +0530

    add readme file

--- src/component/Home.js
+++ src/component/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
       checked
         ? [...prevSelectedItems, item]
         : prevSelectedItems.filter((i) => i !== item)
-    );
+    );  
   };
 
   const handleDelete = () => {
